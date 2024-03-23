adf18f35dc27e1dde9433be04030ac1e7b5b3ae5
yash <yrcb342@gmail.com>
2024-03-23 16:06:50 +0530

    added

---
+++ src/component/footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+import InputButton from './InputButton'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-[345px] bg-[#0D0E10] relative'>
+      <div className="w-[309px] h-[114px] absolute top-[67px] left-[114px] ">
+        <div className="flex flex-col gap-[24px]">
+        <div className="w-full font-montserrat text-[36px] font-[600] leading-[24px] text-left text-[#FFFFFF]">Diligent Insights</div>
+        <div className="flex flex-col gap-[16px] w-[288px] h-[94px]">
+         <div className="w-full font-montserrat text-[16px] font-[400] leading-[24px] text-left text-[#FFFFFF]">Subscribe for Updates</div>
+         <div className="w-[288px] h-[54px] p-[12px] gap-[10px] border border-[1px] rounded-[8px] border-gray-700  opacity-1 flex justify-center items-center">
+           <InputButton/>
+        </div>
+        </div>
+       </div>
+      </div>
+      <div className="absolute w-[105px] h-[24px] top-[228px] left-[114px] font-montserrat text-base font-bold leading-[24px] text-left text-white">Get in touch</div>
+      <div className="absolute w-[calc(100%-548px)]  top-[274px] left-[114px] gap-0  border border-[1px] border-t  border-[#555657] opacity-1"></div>
+
+    </div>
+  )
+}
+
+export default Footer
