8dc0ee1cdfa9e4f4d43c26924af9bcb5d6e2985d
yash <yrcb342@gmail.com>
2024-03-23 14:14:10 +0530

    added

--- src/component/middle.tsx
+++ src/component/middle.tsx
@@ -4,7 +4,7 @@ import Card from './card'
 import Card2 from './Card2'
 const Middle = () => {
   return (
-    <div className='w-full  mt-[48px] flex justify-center items-center'>
+    <div className='w-full  my-[48px] flex justify-center items-center'>
       <div className='flex justify-center items-center gap-[40px]'>
       <Card/>
       <Card2/>
