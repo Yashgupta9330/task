70785982715804d117f8821d85dcbe73558ef84e
Harsh Pandey <pandey73harsh@gmail.com>
2024-03-24 01:31:53 +0530

    last section fixed

--- src/component/Last.tsx
+++ src/component/Last.tsx
@@ -7,7 +7,7 @@ function Last() {
     return (
         <div className=' lg:w-[80%] xl:w-[80%] lg:h-[600px] xl:h-[503px] flex justify-around items-center gap-[20px]  '>
             <div className=' lg:w-[80%] xl:w-[50%] lg:h-[350px] xl:h-[326px] flex flex-col justify-between items-start'>
-                <div className='w-[40%px] lg:h-full xl:h-[80%] flex flex-col justify-around items-start '>
+                <div className='lg:w-[90%] xl:w-[100%] lg:h-full xl:h-[80%] flex flex-col justify-around items-start '>
                     <div className=' lg:w-full xl:w-[90%] flex flex-col justify-between items-start h-[144px] '>
                         <span className='uppercase font-[400] text-[16px]  '>
                             Integration - Coming Soon!
