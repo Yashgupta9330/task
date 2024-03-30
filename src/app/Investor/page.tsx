c934018d2bca2561eba203dc6b02d33b75742e3e
Yashgupta9330 <yaahg342@gmail.com>
2024-03-30 18:46:27 +0530

    added card in 3rd page

--- src/app/Investor/page.tsx
+++ src/app/Investor/page.tsx
@@ -24,9 +24,9 @@ const page = () => {
       <Label htmlFor="name" className=' text-base font-bold text-left '>Description(Optional)</Label>
       <Input type="text" id="name" className=' border border-[#285B52] shadow-md h-[60px] min-w-[50%]' />
       <div className='border border-[#285B52] shadow-md  w-full rounded h-[124px] mb-2 flex items-center gap-4 flex-start pl-4'>
-        <Investorcard/>
-        <Investorcard/>
-        <Investorcard/>
+        <Investorcard head="Company URL" def="www.immersive-engineering.c.." fl='1'/>
+        <Investorcard head="Pitch Deck" def="IE bVildR Pitch Deck.pdf" fl='0'/>
+        <Investorcard head="Company URL" def="IE bVildR Business Plan.pdf" fl='0'/>
         <Button type='submit' className='w-[113px] h-[48px] bg-[#FFFFFF] font-semibold text-[16px]  px-[8px] py-[24px] leading-8 border border-[2px] border-[#7F39EB] rounded-[16px] text-[#7F39EB]'>Analyse</Button>
         <Button type='submit' className='w-[124px] h-[48px] bg-[#7F39EB] font-semibold text-[16px]  px-[8px] py-[24px] leading-8 text-[#FFFFFF] rounded-[16px]'>Generate</Button>
       </div>
--- src/component/investorcard.tsx
+++ src/component/investorcard.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { HiUpload } from "react-icons/hi";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { GrStatusGood } from "react-icons/gr";
 
-const Investorcard = () => {
-    const [fileName, setFileName] = useState('company.com');
-    const [loading,setLoading]=useState(false)
+const Investorcard = ({head,def,fl}) => {
+    const [fileName, setFileName] = useState(def);
+    const [loading, setLoading] = useState(false);
+
     const handleFileUpload = () => {
-        setLoading(true)
+        setLoading(true);
         const input = document.createElement('input');
         input.type = 'file';
         input.addEventListener('change', handleFile);
@@ -12,27 +17,34 @@ const Investorcard = () => {
     };
 
     const handleFile = (e) => {
-        const file = e.target.files[0];
-        console.log(file);
-        setFileName(file.name);
-        setLoading(false)
+        const file =  e.target.files[0];
+        if (file) {
+            setFileName(file.name);
+            setLoading(false);
+        }
     };
 
     return (
-        <div className='h-[91px] w-[216px] flex flex-col gap-2 border items-center justify-center rounded  border-[#BDBDBD] shadow-md'>
-            <div className="text-center">Company URL</div>
-            <div className='text-center w-[99%] border rounded-md  border-[#BDBDBD80] shadow-md'>{fileName}</div>
-            <div className='flex justify-between'>
-             <button onClick={()=>setFileName('company@url')}>DELETE</button>
-             <button onClick={handleFileUpload}>Upload</button>
-             <div className='flex gap-1'>
-                {loading ? (
-                <div>loading</div>
-                ):(
-               <div>success</div>
-                )
-               }
-             </div>
+        <div className='h-[95px] w-[300px] flex flex-col gap-2 border items-center justify-center rounded border-[#BDBDBD] shadow-md py-4'>
+            <div className="text-center">{head}</div>
+            <input type='text' className='text-center w-[99%] border rounded-md border-[#BDBDBD80] shadow-md' value={fileName} onChange={(e) => setFileName(e.target.value)} />
+            <div className={`flex items-center ${fl === '0' ? 'flex-start' : 'flex-end'} w-full justify-between`}>
+                {fl === '0' &&
+                    <>
+                        <RiDeleteBin5Line className='cursor-pointer text-[#DF4C3F]' onClick={() => setFileName(def)} />
+                        <div className="flex justify-center items-center">
+                            <HiUpload className='text-[#7F39EB] cursor-pointer w-4 h-4' onClick={handleFileUpload} />
+                            <div className="cursor-pointer w-[30px] h-[18px]" onClick={handleFileUpload}>Upload</div>
+                        </div>
+                    </>
+                }
+                <div className='flex gap-2 mr-2 justify-center items-center'>
+                    {loading && <AiOutlineLoading3Quarters className='text-[#7F39EB]' />}
+                    <div className='flex justify-center items-center '>
+                        <GrStatusGood className='text-[#285B52]' />
+                        <div>success</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
