8e7191a75fb9a5a1ba3dd0f7a8d42500a7c68df9
YASH GUPTA <yaahg342@gmail.com>
2024-03-30 17:05:51 +0530

    Update inputlabel.tsx

--- src/component/inputlabel.tsx
+++ src/component/inputlabel.tsx
@@ -1,11 +1,9 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export function InputWithLabel({place}) {
+export function InputWithLabel() {
   return (
     <div className="grid w-full items-center gap-1.5">
-      <Label htmlFor="email" className="font-bold text-[16px] leading-[24px] text-[#000000]">{place}</Label>
-      <Input type="email" id="email" placeholder=""  className="h-[50px]"/>
     </div>
   )
 }
