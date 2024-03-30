4577826014a92de1e3499b869b473946c3c3a885
Yashgupta9330 <yaahg342@gmail.com>
2024-03-30 11:58:00 +0530

    added 3rd component

--- src/app/Startup/page.tsx
+++ src/app/Startup/page.tsx
@@ -1,31 +1,25 @@
 import Navbar from '@/component/Navbar'
+import Footer from '@/component/footer'
 import { Button } from '@/components/ui/button'
 import React from 'react'
-//import banner1 from '../../assets/banner1.mp4';
 const page = () => {
   return (
     <div className='w-full flex flex-col'>
     <div className="flex w-full h-[496px]  relative">
     <Navbar/>
-     <div className='absolute w-[650px] h-60 top-52 left-28 flex flex-col gap-14  lg:top-40 lg:left-16'>
+     <div className='absolute w-[650px] h-60 top-52 left-28 flex flex-col gap-14  lg:top-32 lg:left-16'>
       <div className='w-full h-36  text-[56px] font-semibold leading-16 text-[#0D0E10] text-left'>Smart tools to Launch and Scale your ideas</div>
       <div className='flex justify-center items-center w-48 h-14 cursor-pointer'>
       <Button className='w-full h-full bg-[#285B52] font-semibold text-xl text-[#FFFFFF]'>Get Started</Button>
       </div>
      </div>
     </div>
-    <div className="flex justify-center items-center w-full h-[609px] mb-4">
-      <div className='w-[95%] h-full border rounded'>
-      <video
-            muted
-            loop
-            autoPlay
-          >
-          <source src='' type="video/mp4" />
-          Your browser does not support the video tag.
-      </video>
+    <div className="flex justify-center items-center w-full h-[620px] mb-4">
+      <div className='flex justify-center items-center w-[95%] h-full border rounded bg-[#EEEBEA] text-center'>
+        <div className="text-center font-semibold text-[40px] leading-[48px] text-[#0D0E10]"> INSERT DEMO VIDEO</div>
       </div>
-    </div>
+     </div>
+     <Footer/>
     </div>
   )
 }
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
         <div className='w-full bg-[#FFFFFF] h-20 border border-1 flex justify-center items-center'>
         <div className='flex justify-between  w-[95%]'>
-         <div className="flex  justify-center items-center gap-2 cursor-pointer">
+         <div className="flex  justify-center items-center gap-2 lg:gap-1 cursor-pointer">
          <div className="w-6 h-6 bg-[#285B52]"></div>
          <div className='font-montserrat text-2xl  lg:text-3xl 2xl:text-3xl  font-semibold leading-6 text-left text-[#285B52]'>
            Diligent Insights  -  Startup Hub
@@ -15,7 +15,7 @@ const Navbar = () => {
      
         
        
-         <div className="flex  w-[455px] h-12  items-center justify-between  pr-10 pl-10">
+         <div className="flex  w-[455px]  lg:w-[400px] h-12  items-center justify-between  pr-10 pl-10 lg:pr-6 lg:pl-6">
              <div className="flex justify-between gap-8">
                <div className='cursor-pointer'>Home</div>
                <div className='cursor-pointer'>Pricing</div>
