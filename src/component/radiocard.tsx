6d7642f79603f96581b2ba1690ff61ceff469111
Harsh Pandey <pandey73harsh@gmail.com>
2024-03-30 16:35:37 +0530

    responsive

--- src/component/radiocard.tsx
+++ src/component/radiocard.tsx
@@ -14,14 +14,14 @@ interface RadioCardProps {
 
 export const RadioCard: React.FC<RadioCardProps> = ({ heading, content, comingsoon = false, color, textColor }) => {
   return (
-    <Card className="p-[20px] min-w-[170px] min-h-[270px] " style={{backgroundColor: color}} >
+    <Card className="p-[20px] xlarge:p-[40px] min-w-[170px] min-h-[270px] " style={{backgroundColor: color}} >
        <div className={`w-full flex flex-col gap-[20px] text-[#fff]`} style={{color:textColor}}>
        <RadioGroup className="w-[40px] h-[40px] rounded-lg bg-white outline-none border-0 flex items-center justify-center">
        <RadioGroupItem value="default" id="r1" className="w-[20px] h-[20px] outline-none border-0 scale-[2] " />
        </RadioGroup>
-       <h1 className="text-base sm:text-lg font-semibold " >{heading}</h1>
-       <p className=" text-sm sm:text-base" >{content} </p>
-       { comingsoon && <span className=" font-bold text-xl text-center ">Coming Soon!</span> }
+       <h1 className="text-sm sm:text-lg xlarge:text-4xl font-semibold " >{heading}</h1>
+       <p className=" text-xs sm:text-base xlarge:text-2xl" >{content} </p>
+       { comingsoon && <span className=" font-bold text-base sm:text-xl xlarge:text-3xl text-center ">Coming Soon!</span> }
        </div>
     </Card>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,10 +14,19 @@ const config = {
       'sm': '640px',
       // => @media (min-width: 640px) { ... }
 
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
       'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
       'xlarge': '2560px',
-      // => @media (min-width: 1450px) { ... }
+      // => @media (min-width: 2560px) { ... }
     },
     container: {
       center: true,
